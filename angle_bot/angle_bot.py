1af25fc679af79441418554beb61c36323114eec
Mariusz Wróbel <komodo454@wp.pl>
2023-05-22 10:12:09 +0200

    Add checking if ange game has been completed

--- angle_bot/angle_bot.py
+++ angle_bot/angle_bot.py
@@ -41,6 +41,15 @@ def main():
     if is_cookie_popup_active(driver):
         accept_angle_cookies(driver)
 
+    try:
+        WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, f'//h2[normalize-space()="Statistics"]')))
+        print("\nToday's quiz has already been completed.\n")
+        time.sleep(3)
+        driver.quit()
+        return
+    except: 
+        pass
+
     angle_path_element = driver.find_elements(By.TAG_NAME, "path")
 
     angle_path = angle_path_element[2].get_attribute("d")
